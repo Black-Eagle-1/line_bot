24badc686d5fe339a3573fdce9601d34caafcbae
Black Eagle <blackeagle9487@gmail.com>
2020-08-31 10:48:07 +0800

    update

--- app.py
+++ app.py
@@ -40,9 +40,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '我不懂你的意思，輸入[hi]試試吧!'
+
+    if msg == 'hi':
+        r = '哈囉'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+    elif msg == '你幾歲':
+        r = '我的年紀不是人類能計算出來的'
+        
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
